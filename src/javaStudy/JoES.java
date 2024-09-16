b5269693574245e4bf3f29c09c64da9782e10aff
choes0101 <166037587+choes0101@users.noreply.github.com>
2024-09-17 01:22:17 +0900

    init commit

---
+++ src/javaStudy/JoES.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+public class JoES {
+    public static void main(String[] args) {
+        System.out.println("이름 : 조은성");
+        System.out.println("학교 : 가천대학교");
+        System.out.println("전공 : 컴퓨터공학과");
+        System.out.println("학번 : 202334757");
+    }
+}
